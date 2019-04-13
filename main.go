1a771a46a60768d4792811e45883753aa51c1faf
omicron3069 <xlsgoodluck@gmail.com>
2019-04-13 14:40:12 +0800

    I don't want f**king test maybe I screwed up again

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"EvTest/evBus"
 	"fmt"
 	"math/rand"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -13,7 +14,9 @@ import (
 func main() {
 	theFirstBus := evBus.New()
 
-	var wg sync.WaitGroup
+	var (
+		wg sync.WaitGroup
+	)
 	ch1 := make(chan evBus.Event)
 	ch2 := make(chan evBus.Event)
 	ch3 := make(chan evBus.Event)
@@ -23,8 +26,6 @@ func main() {
 	theFirstBus.SubScribe("http:dsb", ch3, printDataEvent)
 	wg.Add(1)
 	go func(topic string, data string) {
-
-		fmt.Println("start 1")
 		for {
 			_ = theFirstBus.Publish(topic, data)
 			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
@@ -34,8 +35,6 @@ func main() {
 
 	wg.Add(1)
 	go func(topic string, data string) {
-
-		fmt.Println("start 2")
 		for {
 			_ = theFirstBus.Publish(topic, data)
 			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
@@ -43,10 +42,8 @@ func main() {
 		//wg.Done()
 	}("http:dsb", "sucker topic2")
 	wg.Wait()
-	fmt.Println("??x")
 }
 
-func printDataEvent(ch evBus.EventChannel) {
-	data := <-ch
-	fmt.Printf("Topic: %s; DataEvent: %v\n", data.Topic, data.Data)
+func printDataEvent(ch reflect.Value) {
+	fmt.Printf("Topic: %s; DataEvent: %v\n", ch.FieldByName("Topic"), ch.FieldByName("Data"))
 }
